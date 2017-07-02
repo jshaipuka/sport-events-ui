83f4572a2985f24595bb4df64a2c897a1dfafbc0
Saipuka <jelena.saipuka@acp.aws.accenture.com>
2017-07-02 21:08:38 +0300

    subscription added form and service

---
+++ src/app/subscription/subscription.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+import { Http } from '@angular/http';
+
+import { Config } from '../app.config';
+
+@Injectable()
+export class SubscriptionService {
+    private base_url = `${Config.API_URL}/subscriptions`;
+
+    constructor(private http: Http) { }
+
+    create(email: string){
+        return this.http.post(this.base_url, { email }).toPromise()
+            .then( status => status )
+            .catch( err => err);
+    }
+}
